286a69291226f0ca762769050ee4173446686b7b
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2022-03-11 14:57:41 +0200

    i made an oopsie

--- commands/admin.py
+++ commands/admin.py
@@ -18,30 +18,30 @@ class Admin:
         pass
 
 
-    async def prune(ctx, times):
+    async def prune(self, times):
 
         #the number of times must be an integer.
         try:
             times = int(times)
         except:
-            await ctx.channel.send("ΣΤΕΙΛΕ ΑΡΙΘΜΟ, ΗΛΙΘΙΕ.")
+            await self.ctx.channel.send("ΣΤΕΙΛΕ ΑΡΙΘΜΟ, ΗΛΙΘΙΕ.")
             return
         
         #if the number of times is a specific meme number, send a message
         if times == 42069 or times == 69420 or times == 69 or times == 420:
-            await ctx.channel.send("Ναι, ναι ΧΑ-ΧΑ. Πολύ αστείο, γαμημένε.")
+            await self.ctx.channel.send("Ναι, ναι ΧΑ-ΧΑ. Πολύ αστείο, γαμημένε.")
             return
         #maximum number of messages to be deleted is 50.
         elif times > 50:
-            await ctx.channel.send("ΤΙ ΛΕΤΕ ΚΥΡΙΕ; ΜΑΞ 50 ΜΗΝΥΜΑΤΑ. ΚΑΙ ΠΟΛΛΑ ΕΙΝΑΙ ΜΗ ΣΟΥ ΠΩ.")
+            await self.ctx.channel.send("ΤΙ ΛΕΤΕ ΚΥΡΙΕ; ΜΑΞ 50 ΜΗΝΥΜΑΤΑ. ΚΑΙ ΠΟΛΛΑ ΕΙΝΑΙ ΜΗ ΣΟΥ ΠΩ.")
             return
         #messages have to be a positive number.
         elif times < 0:
-            await ctx.channel.send("Και για πες, βρε μαλάκα... Πώς θα σβήσω αρνητικό αριθμό μηνυμάτων;")
+            await self.ctx.channel.send("Και για πες, βρε μαλάκα... Πώς θα σβήσω αρνητικό αριθμό μηνυμάτων;")
             return
 
-        await ctx.delete()
-        async for message_to_be_deleted in ctx.channel.history(limit=times):
+        await self.ctx.delete()
+        async for message_to_be_deleted in self.ctx.channel.history(limit=times):
             await message_to_be_deleted.delete()
 
 
